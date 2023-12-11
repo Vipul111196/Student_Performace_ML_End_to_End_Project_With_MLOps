bd1ec1a797fecaddfb6f1559df7e7b56eab6e405
Vipul Tyagi <tyagivipul11.vt@gmail.com>
2023-12-11 19:03:12 +0100

    Logging and Exception commit

---
+++ src/logger.py
@@ -0,0 +1,17 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.mkdir(logs_path, exist_ok= True)
+
+LOG_FILE = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.INFO,
+    format="[%(asctime)s] %(lineno)d - %(name)s - %(levelname)s - %(message)s"
+)
+
+
--- src/utils.py
+++
@@ -1,5 +0,0 @@
-import sys 
-
-def error_message_details(error, error_details:sys):
-    error_detail.exc_info()
-    error_message = "Erro"
