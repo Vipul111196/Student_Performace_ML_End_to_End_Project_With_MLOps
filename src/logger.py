dacbafbaaae17ae9312f007432eb4dcf23c26716
Vipul Tyagi <tyagivipul11.vt@gmail.com>
2023-12-11 19:11:06 +0100

    Second exception and logging commit

--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.mkdir(logs_path, exist_ok= True)
+os.makedirs(logs_path, exist_ok= True)
 
 LOG_FILE = os.path.join(logs_path, LOG_FILE)
 
@@ -14,4 +14,5 @@ logging.basicConfig(
     format="[%(asctime)s] %(lineno)d - %(name)s - %(levelname)s - %(message)s"
 )
 
-
+if __name__ == "__main__":
+    logging.info('Logging has just started')
