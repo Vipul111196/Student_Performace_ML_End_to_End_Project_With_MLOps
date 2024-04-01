a04e16ba2cf7d9118640bbbe9747cc76a5318e98
Vipul Tyagi <tyagivipul11.vt@gmail.com>
2024-04-01 23:19:44 +0200

    added comments

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import os
 import sys
 import pandas as pd
 import numpy as np
-import dill
+import dill #dill is a library similar to pickle but can serialize a wider range of Python objects.
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -12,6 +12,14 @@ def save_object(file_path, obj):
 
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
+
+        '''
+        dill.dump(obj, file_obj): This line uses the dill.dump() 
+        function to serialize the Python object obj and 
+        write it to the file object file_obj. 
+        
+        dill is a library similar to pickle but can serialize a wider range of Python objects.
+        '''
 
     except Exception as e:
         raise CustomException(e, sys)
